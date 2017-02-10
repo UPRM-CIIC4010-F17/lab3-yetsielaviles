c0366d399012862b05e26265720ede23617d5f17
4015b1 <4015b1@parcha.ece.uprm.edu>
2017-02-10 17:14:38 -0400

    Poligon

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -5,7 +5,7 @@ public class Lab3Exercises {
 		JFrame myFrame = new JFrame("Yetsiel");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 400);
+		myFrame.setSize(400, 100);
 		 MyPanelClass myPanel = new MyPanelClass();
          myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
