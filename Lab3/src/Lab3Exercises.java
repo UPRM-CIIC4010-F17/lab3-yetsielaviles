5365b3f25257dd00802975b55f4b238c24e9f1f6
4015f25 <4015f25@granada.ece.uprm.edu>
2017-12-05 10:59:45 -0400

    First commit test

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Yetsiel");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
+		myFrame.setLocation(800, 200);
 		myFrame.setSize(400, 400);
 		 MyPanelClass myPanel = new MyPanelClass();
          myFrame.getContentPane().add(myPanel);
